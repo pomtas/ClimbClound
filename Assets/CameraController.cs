da68232ab7854973559776b0ef590520789fe031
pomtas <pomtas@pop16.odn.ne.jp>
2018-08-05 22:56:17 +0900

    余分なコメント追加

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
+    // playerのGameObject
     GameObject player;
 
     void Start() {
+        // 最初だけ更新
+
+        // GameObjectから"cat"の名前のオブジェクトを探す
         player = GameObject.Find("cat");   
     }
 
-
-
     void Update() {
+        // 毎回更新
+
+        // プレイヤーのポジションを取得
         Vector3 playerPos = this.player.transform.position;
+
+        // 本オブジェクトカメラのY座標をプレイヤーの座標に合わせる
         transform.position = new Vector3(
             transform.position.x, playerPos.y, transform.position.z);
     }
--- Assets/ClearDirector.cs
+++ Assets/ClearDirector.cs
@@ -6,8 +6,12 @@ using UnityEngine.SceneManagement; // LoadScene用
 public class ClearDirector : MonoBehaviour
 {
     void Update() {
+        // 毎回更新
+
+        // マウスボタンが押されたら
         if(Input.GetMouseButtonDown(0))
         {
+            // GameSceneに移動
             SceneManager.LoadScene("GameScene");
         }
     }
